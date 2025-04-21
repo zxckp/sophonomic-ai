2b2e243ae1996206f32eb59ae4c2ae5469229360
zxckp <pricezj@outlook.com>
2025-04-21 10:49:59 +0100

    Fix: Sidebar import and plugin cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    checker({
+      typescript: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    }
+  }
+}
